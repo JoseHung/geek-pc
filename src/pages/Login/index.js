7a356ce4275acadaeb33e8b6aa3bae448d14885d
JoseHung <1309716944@qq.com>
2023-01-13 17:22:42 +0800

    登录表单校验

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,14 +3,30 @@ import './index.scss'
 
 const Login = () => {
     return (
-        <Form>
-            <Form.Item>
+        <Form validateTrigger={['onBlur', 'onChange']}>
+            <Form.Item 
+                name="mobile" 
+                rules={[
+                    {
+                        pattern: /^1[3-9]\d{9}$/,
+                        message: '手机号码格式不对',
+                        validateTrigger: 'onBlur'
+                    },
+                    {require: true, message: '请输入手机号'}
+                ]}
+            >
                 <Input size='large' placeholder='请输入手机号' />
             </Form.Item>
-            <Form.Item>
-                <Input size='large' placeholder='请输入验证码' />
+            <Form.Item 
+                name="code"
+                rules={[
+                    { len: 6, message: '验证码6个字符', validateTrigger: 'onBlur'},
+                    {require: true, message: '请输入验证码'}
+                ]}
+            >
+                <Input size='large' placeholder='请输入验证码' maxLength={6} />
             </Form.Item>
-            <Form.Item>
+            <Form.Item name="remember" valuePropName="checked">
                 <Checkbox className='login-checkbox-label'>
                     我已阅读并同意「用户协议」和「隐私条款」
                 </Checkbox>

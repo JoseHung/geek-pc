7583af9babe57853a7b5610bc54985eb18aa7735
JoseHung <1309716944@qq.com>
2023-01-13 16:15:49 +0800

    创建表单结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,28 @@
-import { Button } from 'antd'
+import { Button, Checkbox, Form, Input } from 'antd';
+import './index.scss'
 
 const Login = () => {
     return (
-        <div>
-            <Button type="primary">Button</Button>
-        </div>
+        <Form>
+            <Form.Item>
+                <Input size='large' placeholder='请输入手机号' />
+            </Form.Item>
+            <Form.Item>
+                <Input size='large' placeholder='请输入验证码' />
+            </Form.Item>
+            <Form.Item>
+                <Checkbox className='login-checkbox-label'>
+                    我已阅读并同意「用户协议」和「隐私条款」
+                </Checkbox>
+            </Form.Item>
+            <Form.Item>
+                <Button type='primary' htmlType='submit' size='large' block>
+                    登录
+                </Button>
+            </Form.Item>
+        </Form>
+        
+        
     )
     
 }

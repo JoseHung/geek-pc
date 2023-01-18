6fcb1c94594ad64b643773d6ecdf57e91f6144bc
JoseHung <1309716944@qq.com>
2023-01-18 17:57:27 +0800

    配置登录mobx

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,5 @@
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input, Card, } from 'antd';
+import logo from '@/assets/logo.png';
 import './index.scss'
 
 const Login = () => {
@@ -6,49 +7,53 @@ const Login = () => {
         console.log(values);
     }
     return (
-        <Form 
-            onFinish={ onFinish } 
-            initialValues={{
-                mobile: '13900000000',
-                code: '246810',
-                remember:  true
-            }}
-            validateTrigger={['onBlur', 'onChange']}>
-            <Form.Item 
-                name="mobile" 
-                rules={[
-                    {
-                        pattern: /^1[3-9]\d{9}$/,
-                        message: '手机号码格式不对',
-                        validateTrigger: 'onBlur'
-                    },
-                    {require: true, message: '请输入手机号'}
-                ]}
-            >
-                <Input size='large' placeholder='请输入手机号' />
-            </Form.Item>
-            <Form.Item 
-                name="code"
-                rules={[
-                    { len: 6, message: '验证码6个字符', validateTrigger: 'onBlur'},
-                    {require: true, message: '请输入验证码'}
-                ]}
-            >
-                <Input size='large' placeholder='请输入验证码' maxLength={6} />
-            </Form.Item>
-            <Form.Item name="remember" valuePropName="checked">
-                <Checkbox className='login-checkbox-label'>
-                    我已阅读并同意「用户协议」和「隐私条款」
-                </Checkbox>
-            </Form.Item>
-            <Form.Item>
-                <Button type='primary' htmlType='submit' size='large' block>
-                    登录
-                </Button>
-            </Form.Item>
-        </Form>
-        
-        
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                {/* 登录表单 */}
+                <Form 
+                    onFinish={ onFinish } 
+                    initialValues={{
+                    mobile: '13900000000',
+                    code: '246810',
+                    remember:  true
+                    }}
+                    validateTrigger={['onBlur', 'onChange']}>
+                    <Form.Item 
+                        name="mobile" 
+                        rules={[
+                        {
+                            pattern: /^1[3-9]\d{9}$/,
+                            message: '手机号码格式不对',
+                            validateTrigger: 'onBlur'
+                        },
+                        {require: true, message: '请输入手机号'}
+                        ]}
+                    >
+                    <Input size='large' placeholder='请输入手机号' />
+                    </Form.Item>
+                    <Form.Item 
+                        name="code"
+                        rules={[
+                            { len: 6, message: '验证码6个字符', validateTrigger: 'onBlur'},
+                            {require: true, message: '请输入验证码'}
+                        ]}
+                    >
+                        <Input size='large' placeholder='请输入验证码' maxLength={6} />
+                    </Form.Item>
+                    <Form.Item name="remember" valuePropName="checked">
+                        <Checkbox className='login-checkbox-label'>
+                            我已阅读并同意「用户协议」和「隐私条款」
+                        </Checkbox>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type='primary' htmlType='submit' size='large' block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
     )
     
 }

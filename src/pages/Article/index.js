b37e5de3270f48e46e854a973ca9932f6831a49d
JoseHung <1309716944@qq.com>
2023-01-29 12:40:40 +0800

    基于Table组件搭建表格结构

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
-import { Breadcrumb, Button, Card, Form, Radio, Select, DatePicker } from 'antd';
+import { Breadcrumb, Button, Card, Form, Radio, Select, DatePicker, Table, Tag, Space } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import img404 from '@/assets/error.png'
 import 'moment/locale/zh-cn'
 import locale from 'antd/es/date-picker/locale/zh_CN';
 
@@ -7,6 +9,69 @@ const { Option } = Select;
 const { RangePicker } = DatePicker;
 
 const Article = () => {
+    const columns = [
+        {
+            title: '封面',
+            dataIndex: 'cover',
+            width: 120,
+            render: cover => {
+                return <img src={cover || img404} width={80} height={60} alt="" />
+            }
+        },
+        {
+            title: '标题',
+            dataIndex: 'title',
+            width: 220
+        },
+        {
+            title: '状态',
+            dataIndex: 'status',
+            render: date => <Tag color="green">审核通过</Tag>
+        },
+        {
+            title: '发布时间',
+            dataIndex: 'pubdate'
+        },
+        {
+            title: '阅读数',
+            dataIndex: 'read_count'
+        },
+        {
+            title: '评论数',
+            dataIndex: 'comment_count'
+        },
+        {
+            title: '点赞数',
+            dataIndex: 'like_count'
+        },
+        {
+            title: '操作',
+            render: data => {
+                return (
+                    <Space size="middle">
+                        <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                        <Button type="primary" danger shape="circle" icon={<DeleteOutlined />} />
+                    </Space>
+                )
+            }
+        }
+    ];
+
+    const data = [
+        {
+            id: '8218',
+            comment_count: 0,
+            cover: {
+                images: ['http://geek.itheima.net/resources/images/15.jpg'],
+            },
+            like_count: 0,
+            pubdate: '2023-01-29 12:27:00',
+            read_count: 2,
+            status: 2,
+            title: 'wkwebview离线化加载h5资源解决方案'
+        }
+    ];
+
     return (
         <div>
             <Card 
@@ -53,6 +118,9 @@ const Article = () => {
                         </Button>
                     </Form.Item>
                 </Form>
+            </Card>
+            <Card title={`根据筛选条件共查询到 count 条结果：`}>
+                <Table rowKey="id" columns={columns} dataSource={data} />
             </Card>
         </div>
     )
